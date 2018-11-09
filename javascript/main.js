2947f5449a761f8206b41406d436c842f21115f6
Slayte Eaton <slayteeaton@Slaytes-MacBook-Pro.local>
2018-11-09 14:20:44 -0600

    working on javascript to get it into the main attraction card

--- javascript/main.js
+++ javascript/main.js
@@ -13,14 +13,14 @@ $.ajax({
        // Grab the results from the API JSON return
        var totalresults = data.total;
        // If our results are greater than 0, continue
-       if (totalresults = 1){
+       if (totalresults > 0){
            // Display a header on the page with the number of results
-           $('#results').append('<h5>We discovered ' + totalresults + ' results!</h5>');
+           //$('#main-attraction-name').append(
            // Itirate through the JSON array of 'businesses' which was returned by the API
-           $.each(data.businesses, function(i, item) {
+          // $.each(data.businesses, function(i, item) {
                // Store each business's object in a variable
                var id = item.id;
-               var alias = item.alias;
+               //var alias = item.alias;
                var phone = item.display_phone;
                var image = item.image_url;
                var name = item.name;
@@ -31,11 +31,11 @@ $.ajax({
                var state = item.location.state;
                var zipcode = item.location.zip_code;
                // Append our result into our page
-               $('#results').append('<div id="' + id + '" style="margin-top:50px;margin-bottom:50px;"><img src="' + image + '" style="width:200px;height:150px;"><br>We found <b>' + name + '</b> (' + alias + ')<br>Business ID: ' + id + '<br> Located at: ' + address + ' ' + city + ', ' + state + ' ' + zipcode + '<br>The phone number for this business is: ' + phone + '<br>This business has a rating of ' + rating + ' with ' + reviewcount + ' reviews.</div>');
-         });
-       } else {
+               $('#attraction-card').append('<div id="#attraction-card"' + id + '<img src="' + image + '" style="width:75px;height:50px;"> <br>We found <b>' + name + '</b> + Business ID: ' + id + ' <br> Located at: ' + address + ' ' + city + ', ' + state + ' ' + zipcode + '<br>The phone number for this business is: ' + phone + '<br>This business has a rating of ' + rating + ' with ' + reviewcount + ' reviews.</div>');
+         //})
+       // } else {
            // If our results are 0; no businesses were returned by the JSON therefor we display on the page no results were found
-           $('#results').append('<h5>We discovered no results!</h5>');
+         //  $('#main-attraction-name').append('<h5>We discovered no results!</h5>');
        }
-   }
-});   
+   })
+}};
